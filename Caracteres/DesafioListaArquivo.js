6e47684b95671c24cfbf086601168fe0d7f14c14
Messias Oliveira <50084447+MessiasOli@users.noreply.github.com>
2022-01-14 07:33:57 -0300

    Desafios

---
+++ Caracteres/DesafioListaArquivo.js
@@ -0,0 +1,4 @@
+// Quantas referencias .mp3 esta dentro do arquivo ?
+const texto = 'Lista de arquivos mp3: jazz.mp3,rock.mp3,podcast.mp3,blues.mp3'
+
+console.log(texto.match(/\.mp3/g))
---
+++ Caracteres/DesafioTresEspacos.js
@@ -0,0 +1,10 @@
+// Encontre os 3 espaços
+
+const texto = 'a   b'
+console.log(texto.match(/a   b/))
+console.log(texto.match(/a\s\s\sb/))
+
+// no futuro
+console.log(texto.match(/a\s+b/))
+console.log(texto.match(/a {3}b/))
+console.log(texto.match(/a\s{3}b/))
